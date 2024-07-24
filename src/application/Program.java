f5205e7560690bebee70cb7f35be40b05c814b5b
rfurlani <furlani.roberto@gmail.com>
2024-07-24 14:30:51 -0300

    Java e MYSQL: Como inserir dados com prepareStatement, executeUpdate, getGeneratedKeys

---
+++ src/application/Program.java
@@ -0,0 +1,71 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Connection conn = null;
+		PreparedStatement st = null, st2 = null;
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"Insert into seller "
+					+ " (Name, Email, BirthDate, BaseSalary, DepartmentId) "
+					+ "VALUES" 
+					+ "(?, ?, ?, ?, ?)",
+					Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, "Karl Marx");
+			st.setString(2, "KMarx@gmail.com");
+			st.setDate(3,  new java.sql.Date(sdf.parse("22/03/1885").getTime()));
+			st.setDouble(4, 14500.00);
+			st.setInt(5, 4);
+			int rowsAffected = st.executeUpdate();
+			
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Done! ID: " + id);
+				}
+			}
+			else {
+				System.out.println("No rows affected!");
+			}
+		
+			st2 = conn.prepareStatement(
+					"Insert into department (Name) Values ('D1'), ('D2')",Statement.RETURN_GENERATED_KEYS);	
+			int rouwsAffected2 = st2.executeUpdate();
+			if (rouwsAffected2 >0) {
+				ResultSet rs1 = st2.getGeneratedKeys();
+				while (rs1.next()) {
+					System.out.println("ID: " + rs1.getInt(1));
+				}
+			}
+			else {
+				System.out.println("No rows affected!");
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ParseException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeStatement(st2);
+			DB.closeConnection();
+		}
+	}
+}
